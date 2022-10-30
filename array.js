3c0141475bc411b6c385790be284ecc7a066e681
shyam <shyamkorade0@gmail.com>
2022-10-30 10:55:02 +0530

    pop add

---
+++ array.js
@@ -0,0 +1,21 @@
+//PUSH array
+const arr = [12, 20, 63, 58]
+console.log(arr)
+
+arr.push(92)
+console.log(arr)
+
+console.log(arr.length)
+
+
+//POP array 
+const poparrayy= ["No friends", "So many friends"]
+console.log(poparrayy)
+
+poparrayy.pop()
+
+
+console.log("Now = "+ poparrayy)
+
+
+
--- push.js
+++
@@ -1,5 +0,0 @@
-const arr = [12, 20, 63, 58]
-console.log(arr)
-
-arr.push(92)
-console.log(arr)
